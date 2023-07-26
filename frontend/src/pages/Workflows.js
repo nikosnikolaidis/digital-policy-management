707819604fba848ac87e10dad36c3858d836ffcf
Nikos Nikolaidis <it14189@uom.edu.gr>
2023-07-26 16:47:37 +0300

    Add example in frontend

--- frontend/src/pages/Workflows.js
+++ frontend/src/pages/Workflows.js
@@ -1,5 +1,25 @@
-const Workflows = () => {
-    return <h1>Workflows</h1>;
+import React, { useState, useRef } from 'react';
+
+
+
+function Workflows(props)  {
+
+    const onClick=(e)=>{
+        i++;
+        setNumber(number+1);
+    }
+
+    const [number, setNumber] = useState(0);
+    
+    var i=0;
+
+    return (
+        <div>
+            <h1>{number}</h1>
+            <h1>{i}</h1>
+            <button onClick={(e)=>onClick(e)}>+</button>
+        </div>
+    );
 };
   
 export default Workflows;
