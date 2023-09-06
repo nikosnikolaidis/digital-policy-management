d66a768ddfab25e445bcaea25a2121dc8add479f
Nikos Nikolaidis <it14189@uom.edu.gr>
2023-09-06 19:39:29 +0300

    Add jbpm in compose and in an iframe

--- frontend/src/pages/Workflows.js
+++ frontend/src/pages/Workflows.js
@@ -1,24 +1,10 @@
 import React, { useState, useRef } from 'react';
 
 
-
 function Workflows(props)  {
 
-    const onClick=(e)=>{
-        i++;
-        setNumber(number+1);
-    }
-
-    const [number, setNumber] = useState(0);
-    
-    var i=0;
-
     return (
-        <div>
-            <h1>{number}</h1>
-            <h1>{i}</h1>
-            <button onClick={(e)=>onClick(e)}>+</button>
-        </div>
+        <iframe src="http://localhost:8181/business-central/kie-wb.jsp" style={{width:"100%", height:"90vh"}}/>
     );
 };
   
