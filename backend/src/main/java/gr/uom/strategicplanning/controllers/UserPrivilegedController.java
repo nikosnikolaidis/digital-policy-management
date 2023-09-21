8f8add87ff1448164e1642cd2444be69b87efe9a
Nikos Nikolaidis <it14189@uom.edu.gr>
2023-09-21 13:48:22 +0300

    Change Controller annotation to RestController annotation

--- backend/src/main/java/gr/uom/strategicplanning/controllers/UserPrivilegedController.java
+++ backend/src/main/java/gr/uom/strategicplanning/controllers/UserPrivilegedController.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
-@Controller
+@RestController
 @RequestMapping("/admin")
 public class UserPrivilegedController {
 
